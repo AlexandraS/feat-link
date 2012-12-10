0166f255630e1cb6a968c542f4eefd50e0d1c3a0
Haru <s0535097@htw-berlin.de>
2012-12-10 14:36:16 +0100

    Das Plugin funktioniert nun mit html-view

--- src/main/java/com/capgemini/hyc/jira/reports/LinkedIssues.java
+++ src/main/java/com/capgemini/hyc/jira/reports/LinkedIssues.java
@@ -1,11 +1,17 @@
 package com.capgemini.hyc.jira.reports;
 
+import com.atlassian.query.Query;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.jira.issue.link.IssueLinkManager;
 import com.atlassian.jira.issue.link.LinkCollection;
+import com.atlassian.jira.issue.search.SearchException;
+import com.atlassian.jira.issue.search.SearchProvider;
+import com.atlassian.jira.issue.search.SearchResults;
+import com.atlassian.jira.jql.builder.JqlQueryBuilder;
 import com.atlassian.jira.plugin.report.impl.AbstractReport;
 import com.atlassian.jira.web.action.ProjectActionSupport;
+import com.atlassian.jira.web.bean.PagerFilter;
 import com.atlassian.jira.util.ParameterUtils;
 
 import java.util.Collection;
@@ -22,24 +28,30 @@ public class LinkedIssues extends AbstractReport {
 	
 	final IssueManager manager;
 	final IssueLinkManager linkManager;
+	final SearchProvider searchProvider;
 	/**
 	 * Konstruktor
 	 * @param manager
 	 * @param linkManager
 	 */
-	public LinkedIssues(final IssueManager manager, final IssueLinkManager linkManager){
+	public LinkedIssues(final IssueManager manager, final IssueLinkManager linkManager, final SearchProvider searchProvider){
 		this.manager = manager;
 		this.linkManager = linkManager;
+		this.searchProvider = searchProvider;
 	}
 	/**
 	 * Diese Methode generiert die html-Ausgabe
 	 */
     @SuppressWarnings("unchecked")
 	public String generateReportHtml(ProjectActionSupport action, Map map) throws Exception {
-      
+      Map <String, Object> linkedIssues = getLinkedIssues(action,map);
     	//return getLinkedIssues(action, map).toString();
     	//die folgende Zeile bitte entkommentieren, um die html-view zu nutzen, und die obere Zeile löschen, oder auskommentieren
-    	return descriptor.getHtml("html-view", getLinkedIssues(action,map));
+      if(linkManager.getLinkCollection(getParams(action, map), action.getLoggedInUser()) == null){
+    	 return ("Kein Objekt");
+      }
+      else{
+      return descriptor.getHtml("html-view", linkedIssues);}
     }
     /**
      * 
@@ -48,13 +60,12 @@ public class LinkedIssues extends AbstractReport {
      * @return Die Methode gibt eine Map, die die Linked Issues zur eingegebenen ID enthält zurück
      */
 	public Map getLinkedIssues(ProjectActionSupport action, Map map){
-    	
-    	LinkCollection linkCollection = linkManager.getLinkCollection(getIssue(action, map), action.getLoggedInUser());
-    	Collection<Issue> linkIssues = linkCollection.getAllIssues();
-    	linkCollection.getInwardIssues(getIssue(action, map).getKey());
-    	//Object[] issues = linkIssues.toArray();
+
+    	LinkCollection linkCollection = linkManager.getLinkCollection(getParams(action, map), action.getLoggedInUser());
+    	Collection<Issue> linkedIssues = linkCollection.getAllIssues();
+    	linkCollection.getInwardIssues(getParams(action, map).getKey());
+    	Object[] issues = linkedIssues.toArray();
     	//List<Issue> linked = null;
-    	//linked.addAll(linkIssues);
     	/**
     	 * In dieser Schleife wird die linked Map mit den Keys der gelinkten Issues gefüllt
     	 */
@@ -69,7 +80,7 @@ public class LinkedIssues extends AbstractReport {
     		/*for(int i =0; i < linked.size(); i++){
     			issueList.add(i, manager.getIssueObject(linked.get(i)));
     		}*/
-    		issueMap.put("linkedIssues", linkIssues);
+    		issueMap.put("linkedIssues", linkedIssues);
     	return issueMap;
     	
     }
@@ -80,12 +91,38 @@ public class LinkedIssues extends AbstractReport {
      * @param action
      * @param map
      * @return Das Issue zur eingegebenen ID
+     * @throws SearchException 
      */
-    public Issue getIssue(ProjectActionSupport action, Map map){
+    public Issue getParams(ProjectActionSupport action, Map map) {
+    	
+    	//JqlQueryBuilder queryBuilder = JqlQueryBuilder.newBuilder();
+    	//Query query = queryBuilder.where().issue(map.get("featID").toString()).buildQuery();
+    	//PagerFilter<Issue> filter = new PagerFilter<Issue>().getUnlimitedFilter();
+    	//SearchResults results = searchProvider.search(query, action.getLoggedInUser(), filter);
     	
     	String featureID = map.get("featID").toString();
     	Issue feature = manager.getIssueObject(featureID);
+    	//queryBuilder.where().issue(linkManager.);
     	return feature;
-    	
+    		
+    }
+    
+    public void validate (ProjectActionSupport action, Map map)
+    {
+    	final String featID =  ParameterUtils.getStringParam(map, "featID");
+    	if (featID == null)
+    	{
+    		action.addError("featID", action.getText("Keine Feature ID eingegeben"));
+    	}
+        if (ParameterUtils.getStringParam(map, "featID").isEmpty())
+        {
+            action.addError("featID", action.getText("Feature ID is Required"));
+        }
+        
+
+    }
+    public boolean showReport()
+    {
+    	return true;
     }
 }
